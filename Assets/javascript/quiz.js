c316f54815fc22ed27eb6af8b72aa04297dd4b7a
Don Tillery <dontillery@Dons-MacBook.local>
2018-03-25 21:43:40 -0400

    Trivia Complete

---
+++ Assets/javascript/quiz.js
@@ -0,0 +1,58 @@
+function submitAnswers(){
+    clearInterval(myVar);
+    var total = 10;
+    var score = 0;
+
+
+
+    var q1 = document.forms["quizForm"]["q1"].value;
+    var q2 = document.forms["quizForm"]["q2"].value;
+    var q3 = document.forms["quizForm"]["q3"].value;
+    var q4 = document.forms["quizForm"]["q4"].value;
+    var q5 = document.forms["quizForm"]["q5"].value;
+    var q6 = document.forms["quizForm"]["q6"].value;
+    var q7 = document.forms["quizForm"]["q7"].value;
+    var q8 = document.forms["quizForm"]["q8"].value;
+    var q9 = document.forms["quizForm"]["q9"].value;
+    var q10 = document.forms["quizForm"]["q10"].value;
+    
+
+for(var i = 1; i <= total; i++){
+    if(eval('q'+i) == null || eval('q'+i) == " "){
+        score--;
+        return false;
+}
+}
+
+var answers = ["a", "c", "c", "d", "b", "b", "a", "c", "d", "a"]
+
+for(i = 1; i <=total; i++){
+
+if (eval('q' + i) == answers[i - 1]){
+    score++;
+}
+
+}
+
+alert("You scored " + score + " out of " + total);
+
+};
+
+
+
+
+var myVar = setInterval(function() {myTimer()
+}, 1000);
+
+ var d = 100;
+ 
+ function myTimer() {
+   document.getElementById("timer").innerHTML = "Time Remaining : " + d-- + " Seconds";
+ 
+   if(d<=-1){
+       alert("Times Up")
+       submitAnswers();
+       clearInterval(myVar); 
+       
+   }
+ };
